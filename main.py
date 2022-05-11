22a206e534f34c9130f09c919dd2c58d99b0b5f5
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-05-11 20:40:23 +1200

    Added Map
    Added a Large map system. Courtesy of SentDex.

--- main.py
+++ main.py
@@ -4,10 +4,76 @@ from sc2.main import run_game
 from sc2.player import Bot, Computer
 from sc2 import maps
 from sc2.ids.unit_typeid import UnitTypeId
+import numpy as np
+import cv2
+import math
 
 class ProBot(BotAI):
     async def on_step(self, iteration: int):
         if self.townhalls.amount == 0 and self.can_afford(UnitTypeId.NEXUS) :
             self.expand_now()
+
+        map = np.zeros((self.game_info.map_size[0], self.game_info.map_size[1], 3), dtype=np.uint8)
+
+        for mineral in self.mineral_field:
+            pos = mineral.position
+            c = [175, 255, 255]
+            fraction = mineral.mineral_contents / 1800
+            if mineral.is_visible:
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+            else:
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [20,75,50]  
+
+
+        for enemy_start_location in self.enemy_start_locations:
+            pos = enemy_start_location
+            c = [0, 0, 255]
+            map[math.ceil(pos.y)][math.ceil(pos.x)] = c
+
+        for enemy_unit in self.enemy_units:
+            pos = enemy_unit.position
+            c = [100, 0, 255]
+            fraction = enemy_unit.health / enemy_unit.health_max if enemy_unit.health_max > 0 else 0.0001
+            map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+
+
+        for enemy_structure in self.enemy_structures:
+            pos = enemy_structure.position
+            c = [0, 100, 255]
+            fraction = enemy_structure.health / enemy_structure.health_max if enemy_structure.health_max > 0 else 0.0001
+            map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+
+        for our_structure in self.structures:
+            if our_structure.type_id == UnitTypeId.NEXUS:
+                pos = our_structure.position
+                c = [255, 255, 175]
+                fraction = our_structure.health / our_structure.health_max if our_structure.health_max > 0 else 0.0001
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+            
+            else:
+                pos = our_structure.position
+                c = [0, 255, 175]
+                fraction = our_structure.health / our_structure.health_max if our_structure.health_max > 0 else 0.0001
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+
+        for vespene in self.vespene_geyser:
+            pos = vespene.position
+            c = [255, 175, 255]
+            fraction = vespene.vespene_contents / 2250
+
+            if vespene.is_visible:
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+            else:
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [50,20,75]
+
+        for our_unit in self.units:
+            pos = our_unit.position
+            c = [175, 255, 0]
+            fraction = our_unit.health / our_unit.health_max if our_unit.health_max > 0 else 0.0001
+            map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+
+
+        cv2.imshow('map',cv2.flip(cv2.resize(map, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST), 0))
+        cv2.waitKey(1)
 
 run_game(maps.get("AcropolisLE"),[Bot(Race.Protoss, ProBot()), Computer(Race.Zerg, Difficulty.Hard)], realtime=False)
